9971b5ac3544ce899a350120bd242d666333af42
snowxxyg <3177867382b@gmail.com>
2026-04-13 18:21:40 +0900

    Delete 03_customer_ltv.sql

--- 03_customer_ltv.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    c.customer_id,
-    COUNT(p.payment_id) AS purchase_count,
-    SUM(p.amount) AS total_spent,
-    AVG(p.amount) AS avg_payment
-FROM customer c
-JOIN payment p ON c.customer_id = p.customer_id
-GROUP BY c.customer_id;
