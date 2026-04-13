195e0cb1240a47df37a20b20c89f323b7854b26c
snowxxyg <3177867382b@gmail.com>
2026-04-13 19:13:56 +0900

    Update 01_top_customers.sql

--- sql/01_top_customers.sql
+++ sql/01_top_customers.sql
@@ -3,8 +3,6 @@ SELECT
     CONCAT(c.first_name, ' ', c.last_name) AS name,
     SUM(p.amount) AS total_spent
 FROM customer c
-JOIN payment p 
-    ON c.customer_id = p.customer_id
+JOIN payment p ON c.customer_id = p.customer_id
 GROUP BY c.customer_id, name
-ORDER BY total_spent DESC
-LIMIT 10;
+ORDER BY total_spent DESC;
